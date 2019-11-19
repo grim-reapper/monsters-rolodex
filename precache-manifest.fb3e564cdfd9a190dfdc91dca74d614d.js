ce4c164fb24f5056efbd849b7ee17b32d0b65fdd
imran ali <imran.ali125@yahoo.com>
2019-11-19 21:22:14 +0500

    Updates

---
+++ precache-manifest.fb3e564cdfd9a190dfdc91dca74d614d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e5e959a494c7bcc84314823131c1b2f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4816f5a3ffdbde9b2cae",
+    "url": "/monsters-rolodex/static/css/main.6ae7e126.chunk.css"
+  },
+  {
+    "revision": "adb3299a92a36b7e19ef",
+    "url": "/monsters-rolodex/static/js/2.7f2be0a3.chunk.js"
+  },
+  {
+    "revision": "4816f5a3ffdbde9b2cae",
+    "url": "/monsters-rolodex/static/js/main.ce53bc49.chunk.js"
+  },
+  {
+    "revision": "bc198eb07b3750b5e64b",
+    "url": "/monsters-rolodex/static/js/runtime-main.b4595895.js"
+  }
+]);
